3dc46974699a28739ff5e6127f7df6e69c45b342
Bogdan Kovalev <bogdan.kovalev@teamdev.com>
2017-01-21 18:08:35 +0200

    logout button

--- uhp-front/app/application/controller.js
+++ uhp-front/app/application/controller.js
@@ -1,5 +1,12 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  navigator: Ember.inject.service()
+  navigator: Ember.inject.service(),
+  session: Ember.inject.service(),
+
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  }
 });
